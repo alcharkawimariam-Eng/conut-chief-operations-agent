6efd1870764b301719e61dbe87b7a3b27ae0ae4e
JuliaIssa2 <jai10@mail.aub.edu>
2026-02-28 11:19:47 +0200

    Create schema.py

---
+++ src/schema.py
@@ -0,0 +1,16 @@
+SALES_COLUMNS = [
+    "date",
+    "branch_id",
+    "item_name",
+    "category",
+    "quantity",
+    "revenue"
+]
+
+ATTENDANCE_COLUMNS = [
+    "date",
+    "branch_id",
+    "employee_id",
+    "shift",
+    "hours"
+]
